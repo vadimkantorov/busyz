01e679ae1596b14dc8e4845b0d577018974aeecb
Vadim Kantorov <vadimkantorov@gmail.com>
2021-05-13 13:54:36 +0300

    ...

--- busyz.c
+++ busyz.c
@@ -9,7 +9,7 @@
 
 #include "miniz.h"
 
-enum { MAX_FILE_PATH_LENGTH = 1024, MAX_EXCLUDE_PATHS = 16, MAX_INPUT_PATHS = 16, USE_FDS = 15 };
+enum { MAX_FILE_PATH_LENGTH = 1024, MAX_EXCLUDE_PATHS = 16, MAX_INPUT_PATHS = 16, USE_FDS = 15, MKDIR_FLAGS = S_IWOTH | S_IROTH | S_IXOTH };
 
 char* exclude[MAX_EXCLUDE_PATHS];
 char* input[MAX_INPUT_PATHS];
@@ -59,7 +59,13 @@ int main(int argc, char *argv[])
     struct stat st;
 
     recurse = num_input = num_exclude = 0;
-    for(int i = 1; i < argc; i++)
+
+    if(argc < 4)
+        return 1;
+
+    int do_zip = 0 == strcmp("zip", argv[1]), do_unzip = 0 == strcmp("unzip", argv[1]);
+
+    for(int i = 2; i < argc; i++)
     {
         if(0 == strcmp("-r", argv[i]))
             recurse = 1;
@@ -68,31 +74,59 @@ int main(int argc, char *argv[])
             assert(i + 1 < argc);
             exclude[num_exclude++] = argv[++i];
         }
+        else if(0 == strcmp("-d", argv[i]))
+        {
+            assert(i + 1 < argc);
+            input[num_input++] = argv[++i];
+        }
         else if(output == NULL)
             output = argv[i];
         else
             input[num_input++] = argv[i];
     }
+    if(do_zip)
+    {
+        remove(output);
 
-	remove(output);
+        ptr_zip = &zip;
+        memset(ptr_zip, 0, sizeof(zip));
+        mz_zip_writer_init_file(ptr_zip, output, 0);
 
-    ptr_zip = &zip;
-	memset(ptr_zip, 0, sizeof(zip));
-    mz_zip_writer_init_file(ptr_zip, output, 0);
-
-    for (int i = 0; i < num_input; i++)
-	{
-        if(stat(input[i], &st) == 0 && (st.st_mode & S_IFDIR) != 0)
+        for (int i = 0; i < num_input; i++)
         {
-            if(recurse == 1)
-                nftw(input[i], proc_entry, USE_FDS, FTW_PHYS);
+            if(stat(input[i], &st) == 0 && (st.st_mode & S_IFDIR) != 0)
+            {
+                if(recurse == 1)
+                    nftw(input[i], proc_entry, USE_FDS, FTW_PHYS);
+            }
+            else
+                proc_entry(input[i], NULL, FTW_F, NULL);
         }
-        else
-            proc_entry(input[i], NULL, FTW_F, NULL);
-	}
 
-    mz_zip_writer_finalize_archive(ptr_zip);
-    mz_zip_writer_end(ptr_zip);
+        mz_zip_writer_finalize_archive(ptr_zip);
+        mz_zip_writer_end(ptr_zip);
+    }
+    else if(do_unzip)
+    {
+        ptr_zip = &zip;
+        memset(ptr_zip, 0, sizeof(zip));
+        const char* inp = output, *outp = num_input > 0 ? input[0] : ".";
+        mkdir(outp, MKDIR_FLAGS);
+        mz_zip_reader_init_file(ptr_zip, inp, 0);
+        int num_files = (int)mz_zip_reader_get_num_files(ptr_zip);
+        for (int file_index = 0; file_index < num_files; file_index++)
+        {
+            mz_zip_archive_file_stat file_stat;
+            mz_zip_reader_file_stat(ptr_zip, file_index, &file_stat);
+            sprintf(file_path_buffer, "%s/%s", outp, file_stat.m_filename);
+            printf("inflating [%s] -> [%s]\n", file_stat.m_filename, file_path_buffer); 
+            if(mz_zip_reader_is_file_a_directory(ptr_zip, file_index))
+                mkdir(file_path_buffer, MKDIR_FLAGS);
+            else
+                mz_zip_reader_extract_to_file(ptr_zip, file_index, file_path_buffer, 0);
+        }
+        mz_zip_reader_end(&ptr_zip);
+    }
 	
     return 0;
 }
